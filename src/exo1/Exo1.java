bb48c9fdb45c373a82e4cba48c3958676d582dab
Utilisateur <Utilisateur@DESKTOP-45UG25C>
2021-10-04 10:33:23 +0200

    Création du dépot

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,27 @@
+// BRUNET Christophe Exercice 1
+
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Bonjour");
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre âge ?");
+        prenom = sc.nextLine();
+    }
+    
+}
